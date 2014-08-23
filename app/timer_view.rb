0ff40c66b52a58eab6abef9f72bc5e35a1cb7619
Len Smith <len@barrison.com>
2014-08-22 21:28:51 -0400

    updated font

--- app/timer_view.rb
+++ app/timer_view.rb
@@ -4,6 +4,7 @@ class TimerView < UIView
   def drawRect(rect)
     super rect
     create_counter_label
+    create_average_label
   end
 
   def create_counter_label
@@ -11,15 +12,34 @@ class TimerView < UIView
     @counter_label.text = "Start"
     @counter_label.frame = self.frame
     @counter_label.adjustsFontSizeToFitWidth = true
-    @counter_label.font = UIFont.fontWithName("Arial", size: 64)
+    @counter_label.font = UIFont.fontWithName("TrebuchetMS", size: 128)
     @counter_label.textAlignment = NSTextAlignmentCenter
 
     self.addSubview(@counter_label)
   end
 
+  def create_average_label
+    @average_label = UILabel.new
+    @average_label.text = "No average yet"
+    @average_label.frame = [[0, self.frame.size.height-100], [320, 100]]
+    @average_label.adjustsFontSizeToFitWidth = true
+    @average_label.font = UIFont.fontWithName("TrebuchetMS", size: 32)
+    @average_label.textAlignment = NSTextAlignmentCenter
+
+    self.addSubview(@average_label)
+  end
+
   def update(coach)
     @counter_label.text = coach.rounds_count.to_s
+    @average_label.text = self.print_time coach.average
     add_progress_view(coach.average) if coach.rounds_count > 0
+  end
+
+  def print_time(seconds)
+    return if seconds.nil?
+    minutes = seconds/60
+    remainder = seconds - (minutes*60)
+    @average_label.text = "#{minutes.to_s.rjust(2, '0')}:#{remainder.to_s.rjust(2, '0')}"
   end
 
   def add_progress_view(seconds)
